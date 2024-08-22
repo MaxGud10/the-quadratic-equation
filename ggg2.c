23c9151b395e440b07a66bbcc2aa1bd99294ab08
MaxGud10 <karatemaksim0110@gmail.com>
2024-08-22 18:21:06 +0300

    добавил защиту для ввода параметров

--- ggg2.c
+++ ggg2.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-  // Определяем константу для бесконечного числа корней
+
+// Определяем константу для бесконечного числа корней
 const double x_min = 1e-6;
 
 enum nRoots { 
@@ -21,18 +22,29 @@ struct str_test {
 
 enum nRoots solve_square(double a, double b, double c, double *x1, double *x2);
 int compare_doubles(double x1, double x2);
-int all_test(struct str_test data, int test_number);// TODO сделать  numtintest_number
+int all_test(struct str_test data, int test_number);
+int clean_bufer();
+
+
 
 int main(void)
 {
-    printf("Square equation solver\n");
-    printf("Enter a, b, c:\n");
-
+   
     double a = NAN, b = NAN, c = NAN;
-    if(scanf("%lf%lf%lf", &a, &b, &c) != 3)
-    { 
-        printf("Error\n");
-        return 0;
+
+    while (1) {
+        printf("Enter coefficients a, b, c:\n");
+        int result = scanf("%lf %lf %lf", &a, &b, &c);
+        //printf("result = %d", result);
+        
+        if (result == 3) {
+            break; 
+        } else {
+            printf("Invalid input, please enter numeric values.\n");
+            clean_bufer();
+            
+            //while(getchar() != '\n');
+        }
     }
     // TODO сделать цикл, чтобы он защищал от неправильного ввода
 
@@ -62,41 +74,52 @@ int main(void)
             return 1;
     }
 
-    struct str_test data1 = {1, -3, 2, 1, 2, TWO_ROOT};  // (x - 2)(x - 1) = 0 --> корни 1, 2
+   struct str_test data1 = {1, -3, 2, 2, 1, TWO_ROOT};  // (x - 2)(x - 1) = 0 --> корни 1, 2
     all_test(data1, 1);
 
-    struct str_test data2 = {1, -5, 6, 2, 3, TWO_ROOT};   // (x - 3)(x - 2) = 0 -> корни 2, 3
+    struct str_test data2 = {1, -5, 6, 3, 2, TWO_ROOT};   // (x - 3)(x - 2) = 0 -> корни 2, 3
     all_test(data2, 2);
 
-    struct str_test data3 = {1, 2, -1, -1, -1, ONE_ROOT};  // (x + 1)^2 = 0 -> корень -1
+    struct str_test data3 = {1, 2, 1, -1, -1, ONE_ROOT};  // (x + 1)^2 = 0 -> корень -1
     all_test(data3, 3);
 
-    struct str_test data4 = {1, 0, -1, -1, 1, TWO_ROOT};  // (x - 1)(x + 1) = 0 -> корни -1, 1
+    struct str_test data4 = {1, 0, -9, -3, 3, TWO_ROOT};  // (x - 3)(x + 3) = 0 -> корни -3, 3
     all_test(data4, 4);
- 
+
     struct str_test data5 = {1, 0, 0, 0, 0, ONE_ROOT};    // x = 0 -> корень 0
     all_test(data5, 5);
 
     struct str_test data6 = {1, 4, 4, -2, -2, ONE_ROOT};   // (x + 2)^2 = 0 -> корень -2
     all_test(data6, 6);
 
-    struct str_test data7 = {2, -4, 2, 2, 2, ONE_ROOT};    // 2x = 4 -> корень 2
+    struct str_test data7 = {0, 2, -4, 2, 2, ONE_ROOT};    // 2x = 4 -> корень 2
     all_test(data7, 7);
-   
 
+    struct str_test data8 = {2, 4, 2, -1, -1, ONE_ROOT};   // 2(x + 1)^2 = 0 -> корень -1
+    all_test(data8, 8);
 
-    //тесты
-    /*all_test(1 ,1, -3, 2, 2, 1, TWO_ROOT);  // (x - 2)(x - 1) = 0 --> корни 1, 2
-    all_test(2 ,1, -5, 6, 3, 2, TWO_ROOT);   // (x - 3)(x - 2) = 0 -> корни 2, 3
-    all_test(3 ,1, 2, 1, -1, -1, ONE_ROOT);  // (x + 1)^2 = 0 -> корень -1
-    all_test(4, 1, 0, -1, -1, 1, TWO_ROOT);  // (x - 1)(x + 1) = 0 -> корни -1, 1
-    all_test(5 ,1, 0, 0, 0, 0, ONE_ROOT);    // x = 0 -> корень 0
-    all_test(6 ,1, 4, 4, -2, -2, ONE_ROOT);   // (x + 2)^2 = 0 -> корень -2
-    all_test(7 ,0, 2, -4, 2, 2, ONE_ROOT);    // 2x = 4 -> корень 2*/
+    struct str_test data9 = {1, 0, 1, NAN, NAN, NO_ROOT}; // x^2 + 1 = 0 -> нет действительных корней
+    all_test(data9, 9);
 
-    
+    struct str_test data10 = {1, -6, 9, 3, 3, ONE_ROOT};   // (x - 3)^2 = 0 -> корень 3
+    all_test(data10, 10);
 
     return 0; 
+}
+int clean_bufer()
+{
+    for (;;)
+    {   
+        //printf("**");
+        int ch = getchar();
+
+        //printf("ya vykkiddyvajjuu %c %d\n", ch, ch);
+        if( ch != '\n' || ch != EOF)
+        {
+            return 0;
+        }
+    }
+    return 0;
 }
 
 int compare_doubles(double x1, double x2) 
@@ -144,22 +167,7 @@ enum nRoots solve_square(double a, double b, double c, double *x1, double *x2)
     }
 }
 
-/*int all_test(int test_number, double a, double b, double c, double x1_expect, double x2_expect, int number_roots_expected)
-{
-    double x1 = NAN, x2 = NAN;
-    enum nRoots number_roots = solve_square(a, b, c, &x1, &x2);
-    
-    if (compare_doubles(number_roots, number_roots_expected) != 0 || 
-        compare_doubles(x1, x1_expect) != 0 || 
-        compare_doubles(x2, x2_expect) != 0)
-        {
-            printf("ErrorTest %d: a = %lf, b = %lf, c = %lf,  number_roots = %d: x1 = %lf, x2 = %lf "
-                   "Expected number_roots = %d: x1 = %lf, x2 = %lf\n", 
-                    test_number, a, b, c, number_roots, x1, x2, number_roots_expected, x1_expect, x2_expect);
-        return 1;
-        }
-    return 0;
-}*/
+
 
 int all_test( struct str_test data, int test_number )
 {
@@ -170,11 +178,17 @@ int all_test( struct str_test data, int test_number )
         compare_doubles(x1, data.x1_expect) != 0 || 
         compare_doubles(x2, data.x2_expect) != 0)
         {
-            printf("Error %d: a = %lf, b = %lf, c = %lf,  number_roots = %d: x1 = %lf, x2 = %lf\n"
-                    "Expected number_roots = %d: x1 = %lf, x2 = %lf\n", 
+            printf("Error %d: a = %lg, b = %lg, c = %lg,  number_roots = %d: x1 = %lg, x2 = %lg\n"
+                    "Expected %d: number_roots = %d: x1 = %lg, x2 = %lg\n", 
                     test_number, data.a, data.b, data.c, number_roots, x1, x2, 
-                    data.number_roots_expected, data.x1_expect, data.x2_expect);
+                    test_number, data.number_roots_expected, data.x1_expect, data.x2_expect);
             return 1;
         }
     return 0;
 }
+
+//напиши проверку на ввод 
+/*while(scanf("%lf%lf%lf", &a, &b, &c) == 3)
+        {
+            scanf("%lf%lf%lf", &a, &b, &c);
+        }*/

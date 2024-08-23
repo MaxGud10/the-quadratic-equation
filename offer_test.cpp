d776c43f1bdc5d10380a56e3e9c539b0051436c5
MaxGud10 <karatemaksim0110@gmail.com>
2024-08-23 22:43:24 +0300

    добавил документацию

--- offer_test.cpp
+++ offer_test.cpp
@@ -1,3 +1,17 @@
+//! @file  
+
+//! функция для проверки unit тестов 
+
+//! функция спрашивает нужны ли тебе unit тесты (YES/NO)
+
+//! @param word массив с длинной CHAR_CONST для ввода ответа пользователя (YES/NO)
+//! @param CHAR_CONST константа для размера массива word
+//! @param data массив из параметров <b> a, b, c </b> для unit тестов
+//! @param Data_number константа для размера массива data
+
+
+
+
 int offer_test( char word[CHAR_CONST], const struct str_test data [Data_number])
 {   
     

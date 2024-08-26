14bb42216d4e039bc95ae11dfe1b33709b9c19be
MaxGud10 <karatemaksim0110@gmail.com>
2024-08-26 13:38:50 +0300

    хедер файл для utils. в нем проверка для коэф, буф

---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+static const double MIN_CONST = 1e-6; // TODO: очень плохое название
+
+int clean_bufer();
+
+int compare_doubles(double x1, double x2);
+
+#endif

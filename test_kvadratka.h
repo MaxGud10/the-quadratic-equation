e94419897ba99a9d56ede8fb6b14a5f9a47d042e
MaxGud10 <karatemaksim0110@gmail.com>
2024-08-27 13:05:28 +0300

    хедер для тестов

---
+++ test_kvadratka.h
@@ -0,0 +1,12 @@
+#ifndef TEST_KVADRATKA_H
+#define TEST_KVADRATKA_H
+
+#include "solve_kvadratka.h"
+
+int run_test(const struct StrTest data [DATA_NUMBER]);
+
+int offer_test(char *word, const struct StrTest data [DATA_NUMBER]);
+
+int unit_test(struct StrTest data, int test_number);
+
+#endif // TEST_KVADRATKA_H

70e7ef760cf65a36dff4be9e1637e7e72d71aa61
MaxGud10 <karatemaksim0110@gmail.com>
2024-08-27 13:07:19 +0300

    хедер для utils

---
+++ utils_kvadratka.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_KVADRATKA_H
+#define UTILS_KVADRATKA_H
+
+static const double ERROR_RATE = 1e-6; 
+
+int clean_bufer();
+
+int compare_doubles(double x1, double x2);
+
+#endif // UTILS_KVADRATKA_H
